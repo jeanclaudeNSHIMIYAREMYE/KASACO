8418b32290c1fa4a85f4c4fc07b07585c47d677f
JeanClaudeNSHIMIYAREMYE <nshimiyaremyejeanclaude1@gmail.com>
2025-11-27 15:44:01 +0200

    modification

--- KASACO/settings.py
+++ KASACO/settings.py
@@ -21,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
+SECRET_KEY = 'django-insecure-rgprxth0kjq2nbkhw-c_hj7_k8#984(q%f5oz=91+(j&s%bhkz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = "voitures.CustomUser"  # Assure-toi que CustomUser est dans l'app 'voiture'
 LOGIN_URL = "login"
@@ -131,14 +130,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
 
 
 
