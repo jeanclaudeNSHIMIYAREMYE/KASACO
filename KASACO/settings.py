4a2e7e235fd9356a5aefae0e0037d8f48bbe4eca
JeanClaudeNSHIMIYAREMYE <nshimiyaremyejeanclaude1@gmail.com>
2025-12-25 14:26:23 +0200

    modification

--- KASACO/settings.py
+++ KASACO/settings.py
@@ -172,14 +172,19 @@ AUTHENTICATION_BACKENDS = [
 # EMAIL CONFIGURATION
 # =========================
 
+
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "nshimiyaremyejeanclaude1@gmail.com"
-EMAIL_HOST_PASSWORD = "emkhwonmgoywrvgn"
-  # mot de passe d’application (16 caractères)
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
-DEFAULT_FROM_EMAIL = "KASACO <nshimiyaremyejeanclaude1@gmail.com>"
+DEFAULT_FROM_EMAIL = os.environ.get(
+    "DEFAULT_FROM_EMAIL",
+    "KASACO <noreply@kasaco.com>"
+)
+
