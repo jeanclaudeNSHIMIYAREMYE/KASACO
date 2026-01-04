b9672e1fb8d29e9fc17a0c0be68b639f29e89194
JeanClaudeNSHIMIYAREMYE <nshimiyaremyejeanclaude1@gmail.com>
2026-01-05 01:12:54 +0200

    depot

--- voitures/forms.py
+++ voitures/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.contrib.auth import authenticate
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.core.exceptions import ValidationError
 
